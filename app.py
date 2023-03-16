5706d4335a7195f89f64c726af141469d6d54076
SuriyaPrakaash2302 <64115301+SuriyaPrakaash2302@users.noreply.github.com>
2023-03-16 19:24:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from prediction import predict
 
 st.title('Velocity Predictor')
 st.text("Part parameters")
-weight = st.slider('Mass', min_value=40, max_value=160, step=10, label_visibility="visible")
-height = st.slider('Height', min_value=30, max_value=200, step=10, label_visibility="visible")
-h_hd = st.slider('H_HD', min_value=0.0, max_value=2.0, step=0.1, label_visibility="visible")
+weight = st.slider('Mass', min_value=40, max_value=160, step=10)
+height = st.slider('Height', min_value=30, max_value=200, step=10)
+h_hd = st.slider('H_HD', min_value=0.0, max_value=2.0, step=0.1)
 
 st.text("Feeder parameters")
-voltage = st.slider('voltage', min_value=0, max_value=259, step=1, label_visibility="visible")
-frequency = st.slider('frequency', min_value=0, max_value=500, step=1, label_visibility="visible")
+voltage = st.slider('voltage', min_value=0, max_value=259, step=1)
+frequency = st.slider('frequency', min_value=0, max_value=500, step=1)
 
 if st.button("Predict"):
     result = predict(np.array([[voltage, frequency, height, h_hd,weight]]))
