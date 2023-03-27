77d544a0b4ff604b9e98407dfcb8f5f91cd630c0
SuriyaPrakaash2302 <64115301+SuriyaPrakaash2302@users.noreply.github.com>
2023-03-27 22:52:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ original_title = '<b style="font-family:Courier; font-size: 20px;"> An advanced 
 st.markdown(original_title, unsafe_allow_html=True)
 
 st.header("Part parameters")
-weight = st.slider('** Mass (in grams) **', min_value=40, max_value=160, step=10, label_visibility="visible")
-height = st.slider('Height (in centimeters)', min_value=30, max_value=200, step=10, label_visibility="visible")
-h_hd = st.slider('Height-Diameter ratio', min_value=0.0, max_value=2.0, step=0.1, label_visibility="visible")
+weight = st.slider('**Mass (in grams)**', min_value=40, max_value=160, step=10, label_visibility="visible")
+height = st.slider('**Height (in centimeters)**', min_value=30, max_value=200, step=10, label_visibility="visible")
+h_hd = st.slider('**Height-Diameter ratio**', min_value=0.0, max_value=2.0, step=0.1, label_visibility="visible")
 
 st.header("Feeder parameters")
-voltage = st.slider('Voltage (Volts)', min_value=0, max_value=259, step=1, label_visibility="visible")
-frequency = st.slider('Frequency (Hertz)', min_value=0, max_value=500, step=1, label_visibility="visible")
+voltage = st.slider('**Voltage (Volts)**', min_value=0, max_value=259, step=1, label_visibility="visible")
+frequency = st.slider('**Frequency (Hertz)**', min_value=0, max_value=500, step=1, label_visibility="visible")
 
 if st.button("Predict"):
 	if (voltage < 100 or frequency<60 or frequency>140 or h_hd<0.2 ):
